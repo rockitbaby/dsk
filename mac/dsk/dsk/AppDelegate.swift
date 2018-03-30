c9e7b4872cec88f26cceeac84bdd6cdb5de335e2
Marius Wilms <mariuswilms@gmx.de>
2018-03-30 21:14:55 +0200

    WIP

---
+++ mac/dsk/dsk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dsk
+//
+//  Created by Marius Wilms on 30.03.18.
+//  Copyright © 2018 Atelier Disko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac/dsk/dsk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dsk
+//
+//  Created by Marius Wilms on 30.03.18.
+//  Copyright © 2018 Atelier Disko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
